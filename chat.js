27183b8018f3c722f795cedbfa9aca4f49583811
Pavan <pavan@192-168-1-107.tpgi.com.au>
2017-04-08 22:48:34 +1000

    core functionality as a baseline

--- chat.js
+++ chat.js
@@ -6,20 +6,28 @@ var socketIO = require('socket.io');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = {};
 	
 
 app.use(express.static(__dirname + "/public"));
 
 io.on('connection', function(clientSocket){
 
+	clientSocket.on('join', function(name){
+		users[clientSocket.id] = name;
+		clientSocket.emit('update', "You have connected to the chat room!!");
+		io.sockets.emit('update', "Hello everybody " + name + " has joined the chat room, send a message!");
+	});
+
 	console.log('User is connected');
 	  
 	clientSocket.on('disconnect', function(){
 		console.log('User is disconnected');
+
 	});
 
 	clientSocket.on('chat-message', function(msg){
-		io.emit('chat-message', 'Server sending to all users: ' + msg);
+		io.emit('chat-message', users[clientSocket.id], msg);
 	});
 });
 
